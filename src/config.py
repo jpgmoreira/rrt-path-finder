f0280f3b3b87c36aee60f375910175f5d56e3b11
JoaoPedro <jpgmoreira19@gmail.com>
2020-05-08 19:11:52 -0300

    Changes on config.py .

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ WIDTH = 800
 HEIGHT = 600
 
 # Radius of the start and goal circles:
-RADIUS = 30
+RADIUS = 20
 
 START_INIT_POS = (10, 10)
 GOAL_INIT_POS = (WIDTH - 10, HEIGHT - 10)
@@ -15,7 +15,7 @@ START_COLOR = (0, 255, 0)
 GOAL_COLOR = (255, 0, 0)
 
 OBSTACLES_COLOR = (77, 135, 181)
-OBSTACLES_RADIUS = 15
+OBSTACLES_RADIUS = 10
 
 # During RRT, update the screen every MAX_EDGES_POOL new edges created.
 MAX_EDGES_POOL = 10
